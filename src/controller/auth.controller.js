e1904213b4e88cdc2ffef77b80f3e4942778edfc
Harsh Chauhan <hc063213@gmail.com>
2026-08-18 14:30:55 +0530

    Added Email Sender functionality which sends email to new registered users

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const emailService = require("../services/email.service")
 /**
  * - User register Controller
  * - POST /api/auth/register
@@ -33,6 +34,8 @@ async function registerUserController (req, res){
         },
         token
     })
+    await emailService.sendRegistrationEmail(user.email, user.name)
+
 
 
 }
