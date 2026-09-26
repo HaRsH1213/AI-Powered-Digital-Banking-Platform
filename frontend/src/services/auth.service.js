00993ba68cfff3f36b3265af895fda6f0ece2359
Harsh Chauhan <hc063213@gmail.com>
2026-09-26 22:56:11 +0530

    added auth service for registration, email verification and resend otp

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -11,4 +11,19 @@ export const logout = async () => {
     return await api.post("/auth/logout")
 }
 
+export const register = async (userData) =>{
+    const response = await api.post("/auth/register", userData)
+    return response.data.user
+}
+
+export const verifyEmail = async (otp) => {
+    const response = await api.post("/auth/verify-email", { otp })
+    return response.data
+}
+
+export const resendOtp = async () => {
+    const response = await api.post("/auth/resend-otp")
+    return response.data
+}
+
 // export default {login, logout}
